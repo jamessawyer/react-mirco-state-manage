cbeb16bf874ef4b5569b462a3cdd05b3b1781d6b
jamessawyer <550961784@qq.com>
2022-04-23 21:25:31 +0800

    使用全局状态 + set收集更新函数的方式 解决状态不同步的问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,69 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+
+const createContainer = (initialState: unknown) => {
+  let state = initialState
+  const getState = () => state
+  const setState = (nextState: unknown) => {
+    state = typeof nextState === 'function' ? nextState(state) : nextState
+  }
+
+  return { getState, setState }
+}
+
+let count = 0
+
+// 为了解决 Counter1 & Counter2 中 state 不能同步更新的问题
+// 使用一个set 将所有的 setState 收集起来，然后点击时使用 forEach 调用所有 setStateFunctions 中的 setState
+// 但是这样会出现大量重复性的代码，可以使用订阅的方式去解决这个问题
+let setStateFunctions = new Set<(count: number) => void>()
+
+const Counter1 = () => {
+  const [state, setState] = useState(count)
+
+  useEffect(() => {
+    setStateFunctions.add(setState)
+    return () => {
+      setStateFunctions.delete(setState)
+    }
+  }, [])
+
+  const inc = () => {
+    count += 1
+    setStateFunctions.forEach(fn => fn(count))
+  }
+
+  return <div>
+    state : {state}
+    <button onClick={inc}>+1</button>
+  </div>
+}
+
+const Counter2 = () => {
+  const [state, setState] = useState(count)
+  useEffect(() => {
+    setStateFunctions.add(setState)
+    return () => {
+      setStateFunctions.delete(setState)
+    }
+  }, [])
+
+  const inc = () => {
+    count += 2
+    setStateFunctions.forEach(fn => fn(count))
+  }
+
+  return <div>
+    state2 : {state}
+    <button onClick={inc}>+2</button>
+  </div>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Counter1 />
+      <Counter2 />
     </div>
   )
 }
