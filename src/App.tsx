ade6f4c920a6d389c630cdf374d731310d4843b2
jamessawyer <550961784@qq.com>
2022-04-23 22:39:07 +0800

    添加 useStoreSelector 用于提取store中的部分state， 避免其余订阅了store的组件重渲染

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 const createContainer = (initialState: unknown) => {
   let state = initialState
@@ -37,7 +37,7 @@ const createStore = <T extends unknown>(initialState: T): Store<T> => {
   return { getState, setState, subscribe }
 }
 
-const store = createStore({ count: 0 })
+const store = createStore({ count1: 1, count2: 2 })
 // console.log('get state: ', store.getState())
 // store.setState({ count: 1 })
 // console.log('get state 2: ', store.getState())
@@ -64,45 +64,72 @@ const useStore = <T extends unknown>(store: Store<T>) => {
   return [state, store.setState] as const
 }
 
+// 因为上面的 useStore 返回整个 state 对象
+// 这样会导致 state中的任何一部分状态发生了更新，其余订阅了store的组件 都会重新渲染 导致性能问题
+// 所以我们需要把 state 中的某个部分抽离出来
+const useStoreSelector = <T, S>(store: Store<T>, selector: (state: T) => S) => {
+  const [state, setState] = useState(() => selector(store.getState()))
+
+  useEffect(() => {
+    const unsubscribe = store.subscribe(() => {
+      setState(selector(store.getState()))
+    })
+
+    setState(selector(store.getState()))
+
+    return unsubscribe
+  }, [store, selector])
+
+  return state
+}
+
 
 
 const Counter1 = () => {
-  const [state, setState] = useStore(store)
+  // 使用 useCallback 获取一个稳定的 selector 函数
+  const state = useStoreSelector(store, useCallback(state => state.count1, []))
 
   const inc = () => {
-    setState(prev => ({
+    // 注意这里需要使用 immutable 的方式更新对象
+    store.setState(prev => ({
       ...prev,
-      count: prev.count + 1
+      count1: prev.count1 + 1
     }))
   }
 
   return <div>
-    state : {state.count}
+    state : {state}
     <button onClick={inc}>+1</button>
   </div>
 }
+
+// selector 函数 只选取部分状态
+const selectCount2 = (state: ReturnType<typeof store.getState>) => state.count2
+
 const Counter2 = () => {
-  const [state, setState] = useStore(store)
+  const state = useStoreSelector(store, selectCount2)
 
   const inc = () => {
-    setState(prev => ({
+    store.setState(prev => ({
       ...prev,
-      count: prev.count + 2
+      count2: prev.count2 + 1
     }))
   }
 
   return <div>
-    state2 : {state.count}
-    <button onClick={inc}>+2</button>
+    state2 : {state}
+    <button onClick={inc}>+1</button>
   </div>
 }
 
-
 function App() {
 
+  // 使用 selector 之后，Counter1中的 count1 更新不会导致 Counter2组件重渲染
   return (
     <div className="App">
       <Counter1 />
+      <Counter1 />
+      <Counter2 />
       <Counter2 />
     </div>
   )
