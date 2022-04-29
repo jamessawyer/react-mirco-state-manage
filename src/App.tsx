b4c46f577792e63881ea7373b7bb002d5f1588b1
jamessawyer <550961784@qq.com>
2022-04-29 23:04:22 +0800

    Jotai 可使用 read函数派生新的atom,派生的atom则会随着依赖的atoms自动更新

--- src/App.tsx
+++ src/App.tsx
@@ -25,12 +25,40 @@ const Counter2 = () => {
   )
 }
 
+const firstNameAtom = atom('React')
+const lastNameAtom = atom('Native')
+const ageAtom = atom(19)
+
+// Jotai 中派生 atom 的概念
+// get => {}  read 函数， 可以获取到其它原子的值
+// 如果 firstNameAtom | lastNameAtom | ageAtom 发生变化，派生的 personAtom 也会自动变化
+// 这称之为 依赖追踪，Jotai帮我们自动进行管理
+const personAtom = atom(get => ({
+  firstName: get(firstNameAtom), // get(firstNameAtom) 获取 firstNameAtom 的值
+  lastName: get(lastNameAtom),
+  age: get(ageAtom)
+}))
+
+const PersonComponent = () => {
+  const [person] = useAtom(personAtom)
+
+  return (
+    <>
+    firstName: <h3>{person.firstName}</h3>
+    lastName: <h3>{person.lastName}</h3>
+    age: <h3>{person.age}</h3>
+    </>
+  )
+}
+
 function App() {
 
   return (
     <div className="App">
       <Counter1 />
       <Counter2 />
+      <hr />
+      <PersonComponent />
     </div>
   )
 }
